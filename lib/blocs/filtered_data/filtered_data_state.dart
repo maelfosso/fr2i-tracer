e8b2da69cba3b6a89f7bb535c84c0149135e255b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-11-13 00:36:15 +0100

    filtered data state added

---
+++ lib/blocs/filtered_data/filtered_data_state.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+import 'package:tracer/models/models.dart';
+
+abstract class FilteredDataState extends Equatable {
+  const FilteredDataState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FilteredDataLoadInProgress extends FilteredDataState {}
+
+class FilteredDataLoadSuccess extends FilteredDataState {
+  final List<Todo> filteredData;
+  final VisibilityFilter activeFilter;
+
+  const FilteredDataLoadSuccess(
+    this.filteredData,
+    this.activeFilter,
+  );
+
+  @override
+  List<Object> get props => [filteredData, activeFilter];
+
+  @override
+  String toString() {
+    return 'FilteredDataLoadSuccess { filteredData: $filteredData, activeFilter: $activeFilter }';
+  }
+}
