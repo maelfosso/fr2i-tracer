046570d92349396c1a282880862787049b3e8966
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-11-21 06:49:36 +0100

    home design changed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,86 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tracer/blocs/data/data_bloc.dart';
-import 'package:tracer/screens/classify_picture_screen.dart';
 import 'package:tracer/screens/keys.dart';
 
 class HomeScreen extends StatelessWidget {
-
-  Widget _buildFeatureItem(IconData icon, String name, VoidCallback callback) {
-    return Card(
-      child: InkWell(
-        onTap: callback,
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-          child: Column(
-            children: [
-              Icon(
-                icon,
-                size: 32.0,
-                semanticLabel: name,
-              ),
-              Text(name)
-            ],
-          ),
-        )
-      )
-    );
-  }
-
-  Widget _imagesFeatures(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Container(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "Pictures",
-            style: Theme.of(context).textTheme.headline5
-          ),
-        ),
-        Wrap(
-          direction: Axis.horizontal,
-          alignment: WrapAlignment.spaceBetween,
-          children: [
-            _buildFeatureItem(Icons.add_a_photo ,"Take a picture", () {
-              Navigator.pushNamed(context, ArchSampleRoutes.takePicture);
-            }),
-            _buildFeatureItem(Icons.send, "Send a picture", () {
-              print("Taped on TAKE A PICTURE");
-            }),
-            _buildFeatureItem(Icons.toys, "Classify a picture", () {
-              Navigator.pushNamed(context, ArchSampleRoutes.classifyPicture);
-            })  
-          ]
-        )
-      ]
-    );
-  }
-
-  Widget _dataFeatures(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "Data",
-            style: Theme.of(context).textTheme.headline5
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            _buildFeatureItem(Icons.add ,"Enter a data", () {
-              Navigator.pushNamed(context, ArchSampleRoutes.addData);
-            }),
-            _buildFeatureItem(Icons.all_out, "All your data", () {
-              Navigator.pushNamed(context, ArchSampleRoutes.listData);
-            })
-          ]
-        )
-      ]
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -92,10 +15,95 @@ class HomeScreen extends StatelessWidget {
         ),
         body: Container(
           padding: EdgeInsets.all(16.0),
-          child: Column(
+          child: ListView(
             children: [
-              _imagesFeatures(context),
-              _dataFeatures(context)
+              ListTile(
+                title: Text(
+                  "Pictures",
+                  style: Theme.of(context).textTheme.headline5
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.add_a_photo,
+                  size: 32.0,
+                ),
+                title: Text(
+                  "Take a picture",
+                  // style: Theme.of(context).textTheme.headline5
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, ArchSampleRoutes.takePicture);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.send,
+                  size: 32.0,
+                ),
+                title: Text(
+                  "Send a picture",
+                  // style: Theme.of(context).textTheme.headline5
+                ),
+                onTap: () {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      duration: Duration(seconds: 3),
+                      content: Text(
+                        "Feature not implemented",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis
+                      )
+                    )
+                  );
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.toys,
+                  size: 32.0,
+                ),
+                title: Text(
+                  "Classify a picture",
+                  // style: Theme.of(context).textTheme.headline5
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, ArchSampleRoutes.classifyPicture);
+                },
+              ),
+
+
+              ListTile(
+                title: Text(
+                  "Data",
+                  style: Theme.of(context).textTheme.headline5
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.add_a_photo,
+                  size: 32.0,
+                ),
+                title: Text(
+                  "Enter a data",
+                  // style: Theme.of(context).textTheme.headline5
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, ArchSampleRoutes.addData);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.send,
+                  size: 32.0,
+                ),
+                title: Text(
+                  "All your data",
+                  // style: Theme.of(context).textTheme.headline5
+                ),
+                onTap: () {
+                  Navigator.pushNamed(context, ArchSampleRoutes.listData);
+                },
+              ),            
             ],
           )
         )
